914917e31fc93aa0728326d5182b928ca856321b
astraszab <astarshab1@gmail.com>
2020-10-21 13:32:11 +0300

    Add notes model and database

---
+++ app/src/main/java/com/example/livret/data/Note.kt
@@ -0,0 +1,17 @@
+package com.example.livret.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    var noteId: Long = 0L,
+
+    @ColumnInfo(name = "title")
+    var title: String = "Title",
+
+    @ColumnInfo(name = "content")
+    var content: String = "Content"
+)
---
+++ app/src/main/java/com/example/livret/data/NoteDatabaseDao.kt
@@ -0,0 +1,26 @@
+package com.example.livret.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Insert
+    fun insert(note: Note)
+
+    @Update
+    fun update(note: Note)
+
+    @Query("SELECT * from note_table WHERE noteId = :key")
+    fun get(key: Long): Note?
+
+    @Query("DELETE FROM note_table")
+    fun clear()
+
+    @Query("SELECT * FROM note_table ORDER BY noteId DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+}
